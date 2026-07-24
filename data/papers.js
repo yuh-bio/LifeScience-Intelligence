05a1407da3d2121eeec885a2e2df30ebc02d09c6
yuh-bio <yuh7012@gmail.com>
2026-07-24 09:54:35 +0800

    Create papers.js

---
+++ data/papers.js
@@ -0,0 +1,68 @@
+export const papers = [
+
+{
+title:
+"Genome editing with CRISPR-Cas9",
+
+authors:
+"Jennifer Doudna, Emmanuelle Charpentier",
+
+journal:
+"Science",
+
+year:
+"2012",
+
+field:
+"Gene Editing",
+
+doi:
+"10.1126/science.1225829"
+
+},
+
+
+{
+title:
+"Single-cell transcriptomic profiling",
+
+authors:
+"Human Cell Atlas Research Team",
+
+journal:
+"Nature",
+
+year:
+"2019",
+
+field:
+"Single-cell Sequencing",
+
+doi:
+"10.xxxx/example"
+
+},
+
+
+{
+title:
+"Human organoids for disease modeling",
+
+authors:
+"Biomedical Research Group",
+
+journal:
+"Nature Medicine",
+
+year:
+"2020",
+
+field:
+"Organoid",
+
+doi:
+"10.xxxx/example"
+
+}
+
+];
